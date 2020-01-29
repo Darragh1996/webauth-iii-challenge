94c0f1bd99dc7a7eda4f7d42ad825a1deb43ce21
Darragh Ferry <darraghferry@hotmail.com>
2020-01-29 15:25:53 +0000

    set up auth router

--- api/api-router.js
+++ api/api-router.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
 
+const authRouter = require("../routers/auth-router");
+
+router.use("/auth", authRouter);
+
 router.get("/", (req, res) => {
   res.json("SERVER IS UP AND RUNNING");
 });
